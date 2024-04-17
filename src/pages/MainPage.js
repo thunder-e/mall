e29521432a44db4ae2e87893ff1a214dfcfe9a4d
[thunder-e] <[pinkpear0114@naver.com]>
2024-04-17 15:10:23 +0900

    front

---
+++ src/pages/MainPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import BasicLayout from "../layouts/BasicLayout";
+
+function MainPage(props) {
+  return (
+    <BasicLayout>
+      <div className={"text-3xl"}>Main Page</div>
+    </BasicLayout>
+  );
+}
+
+export default MainPage;
