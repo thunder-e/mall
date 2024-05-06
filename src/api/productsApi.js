b3b74f557c777b8fb982faa122c6910d24a35855
[thunder-e] <[pinkpear0114@naver.com]>
2024-05-06 17:40:08 +0900

    :sparkle: 리덕스툴킷 설정, 쿠키처리

--- src/api/productsApi.js
+++ src/api/productsApi.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 import { API_SERVER_HOST } from "./todoApi";
+import jwtAxios from "../util/jwtUtil";
 
 const host = `${API_SERVER_HOST}/api/products`;
 
 export const postAdd = async (product) => {
   const header = { headers: { "Content-Type": "multipart/form-data" } };
 
-  const res = await axios.post(`${host}/`, product, header);
+  // 경로 뒤 '/' 주의
+  const res = await jwtAxios.post(`${host}/`, product, header);
 
   return res.data;
 };
@@ -14,21 +16,15 @@ export const postAdd = async (product) => {
 export const getList = async (pageParam) => {
   const { page, size } = pageParam;
 
-  const res = await axios.get(`${host}/list`, {
+  const res = await jwtAxios.get(`${host}/list`, {
     params: { page: page, size: size },
   });
 
   return res.data;
 };
 
-export const getOne = async (pno) => {
-  const res = await axios.get(`${host}/${pno}`);
-
-  return res.data;
-};
-
-export const deleteOne = async (pno) => {
-  const res = await axios.delete(`${host}/${pno}`);
+export const getOne = async (tno) => {
+  const res = await jwtAxios.get(`${host}/${tno}`);
 
   return res.data;
 };
@@ -36,7 +32,13 @@ export const deleteOne = async (pno) => {
 export const putOne = async (pno, product) => {
   const header = { headers: { "Content-Type": "multipart/form-data" } };
 
-  const res = await axios.put(`${host}/${pno}`, product, header);
+  const res = await jwtAxios.put(`${host}/${pno}`, product, header);
+
+  return res.data;
+};
+
+export const deleteOne = async (pno) => {
+  const res = await jwtAxios.delete(`${host}/${pno}`);
 
   return res.data;
 };
--- src/slices/loginSlice.js
+++ src/slices/loginSlice.js
@@ -1,8 +1,15 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { loginPost } from "../api/memberApi";
+import { getCookie, removeCookie, setCookie } from "../util/cookieUtil";
 
 const initState = {
   email: "",
+};
+
+const loadMemberCookie = () => {
+  const memberInfo = getCookie("member");
+
+  return memberInfo;
 };
 
 export const loginPostAsync = createAsyncThunk("loginPostAsync", (param) =>
@@ -11,14 +18,14 @@ export const loginPostAsync = createAsyncThunk("loginPostAsync", (param) =>
 
 const loginSlice = createSlice({
   name: "loginSlice",
-  initialState: initState,
+  initialState: loadMemberCookie() || initState,
   reducers: {
     login: (state, action) => {
       console.log("login.......action : ", action);
       return { email: action.payload.email }; //새로운상태 -> useSelector
     },
     logout: () => {
-      console.log("logout......");
+      removeCookie("member");
       return { ...initState };
     },
   },
@@ -28,6 +35,10 @@ const loginSlice = createSlice({
         console.log("fullfilled");
 
         const payload = action.payload;
+
+        if (!payload.error) {
+          setCookie("member", JSON.stringify(payload), 1);
+        }
 
         return payload;
       })
